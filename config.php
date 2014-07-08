a5c98db1c983882d818b6ea0646a37f67651d01f
Rich Hatch <rhatch@w2odigital.com>
2014-07-08 18:53:37 -0500

    init commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'sYaoiwAotKZNbQPrAfotXA');
+define('CONSUMER_SECRET', '2aNJfSHqASxDd6y26fkUizzgQK6azckTpqza3qg');
+define('OAUTH_CALLBACK', 'http://www.bbabros.com/twitter/callback.php');
